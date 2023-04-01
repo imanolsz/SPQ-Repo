d05cf5b5a9ba387aada199a5014c1779418a9dc0
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2023-04-01 17:37:35 +0200

    Base de datos clientes

--- sql/create-messages.sql
+++ sql/create-messages.sql
@@ -10,6 +10,15 @@ CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
 
 GRANT ALL ON messagesDB.* TO 'spq'@'localhost';
 
+/* Tabla Cliente */
+CREATE TABLE IF NOT EXISTS Clientes ( /* Creamos la tabla de clientes solo sino existe */
+id INTEGER PRIMARY KEY,
+nombre TEXT NOT NULL,
+password TEXT NOT NULL 
+);
+
+
+
 /* Tabla Reservas */
 CREATE TABLE IF NOT EXISTS Reservas ( /* Creamos la tabla de reservas solo sino existe */
   id INTEGER PRIMARY KEY,
@@ -17,5 +26,7 @@ CREATE TABLE IF NOT EXISTS Reservas ( /* Creamos la tabla de reservas solo sino 
   fecha TEXT,
   hora TEXT,
   cantidad_personas INTEGER,
-  Cancelada BOOLEAN /* Si esta cancelado se pone atrue, sino la reseva esta confirmada por defecto */ 
+  Cancelada BOOLEAN, /* Si esta cancelado se pone atrue, sino la reseva esta confirmada por defecto */ 
+  FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
 );
+
