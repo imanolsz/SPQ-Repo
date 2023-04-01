07aa00533e4013c853c032eeb48442d635af6a18
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2023-04-01 13:05:06 +0200

    Ventana Registro y principal

---
+++ src/main/java/es/deusto/spq/ventanas/GestorVentana.java
@@ -0,0 +1,30 @@
+public class GestorVentanas {
+	
+	VentanaInicio VentanaInicioSesion;
+	VentanaPrincipal VentanaPrincipal;
+	VentanaRegistro VentanaRegistro;
+	VentanaMenu VentanaMenu;
+	
+	public GestorVentanas() {
+		VentanaPrincipal = new VentanaPrincipal();
+		VentanaInicioSesion = new VentanaInicio();
+		VentanaRegistro = new VentanaRegistro();
+		VentanaMenu = new VentanaMenu();
+	}
+	
+	public VentanaPrincipal getVentanaPrincipal() {
+		return VentanaPrincipal;
+	}
+
+	public VentanaInicio getVentanaInicio() {
+		return VentanaInicioSesion;
+	}
+	
+	public VentanaRegistro getVentanaRegistro() {
+		return VentanaRegistro;
+	}
+	
+	public VentanaMenu getVentanaMenu() {
+		return VentanaMenu;
+	}
+}
