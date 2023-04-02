7555a0927e2f5099ec156603e3e3ff72678ed7e7
Roberto Fernández Barrios <roberto.fernandez@opendeusto.es>
2023-04-02 19:10:20 +0200

    Update

--- src/main/java/es/deusto/spq/client/ExampleClient.java
+++ src/main/java/es/deusto/spq/client/ExampleClient.java
@@ -108,22 +108,26 @@ public class ExampleClient {
 		return notifications; // Devuelve la lista, aunque esté vacía si hay un error
 	}
 	
-	public void getReservas() {
+	public List<ReservaData> getReservas() {
 		WebTarget getReservasWebTarget = webTarget.path("admin/getReservas"); // Crea un objeto WebTarget con la URL del servicio REST que se desea invocar
 		Invocation.Builder invocationBuilder = getReservasWebTarget.request(MediaType.APPLICATION_JSON); //  Se crea un objeto Invocation.Builder, que se utiliza para configurar la solicitud REST
 		Response response = invocationBuilder.get();  // Se realiza la solicitud REST utilizando el método get() del objeto invocationBuilder, y se almacena la respuesta en un objeto Response.
 		if (response.getStatus() != Status.OK.getStatusCode()) { // Se verifica si la respuesta de la solicitud es exitosa, si no es exitosa, se registra un error en el archivo de registro.
 			logger.error("Error connecting with the server. Code: {}", response.getStatus()); 
+			throw new RuntimeException("Error connecting with the server. Code: " + response.getStatus());
 		} else {
 			try {
 				List<ReservaData> reservas = response.readEntity(new GenericType<List<ReservaData>>() {}); //Se lee la respuesta en formato JSON y se convertierte en una lista de objetos ReservaData. Se utiliza un objeto GenericType para especificar el tipo de objeto que se está convirtiendo.
+				if (reservas == null) {
+					throw new RuntimeException("Null list of ReservaData returned from the server");
+				}
 				logger.info("* Reservas obtenidas: '{}'", reservas); // Se registra un mensaje de información en el archivo de registro con la lista de reservas obtenidas
+				return reservas;
 			} catch (Exception e) {
 				logger.error("Error al obtener las reservas: ", e);
+				throw new RuntimeException("Error al obtener las reservas", e);
 			}
 		}
 	}
-	
-	
 
 }
