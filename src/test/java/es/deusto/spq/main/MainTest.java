e946157d6e5ba9ecba21eadea2ec73dc2100a8e0
Roberto Fernández Barrios <roberto.fernandez@opendeusto.es>
2023-05-03 01:42:09 +0200

    junits

--- src/test/java/es/deusto/spq/main/MainTest.java
+++ src/test/java/es/deusto/spq/main/MainTest.java
@@ -1,29 +1,39 @@
 package es.deusto.spq.main;
 
-import static org.junit.Assert.*;
-import org.junit.*;
+import es.deusto.spq.client.ExampleClient;
+import es.deusto.spq.client.gui.GestorVentanas;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 
 public class MainTest {
 
-	
-	// private Main main;
-    
-	// @Before
-    // public void setUp() {
-    //     String[] args = {"127.0.0.1", "8080"};
-    //     main = new Main();
-    //     Main.main(args);
-    // }
-	
-	// @Test
-    // public void testExampleClient() {
-    //     assertNotNull(Main.getExampleClient());
-    // }
+    private GestorVentanas gestorVentanas;
+    private ExampleClient exampleClient;
 
-	// @Test
-    // public void testGestorVentanas() {
-    //     assertNotNull(Main.getGestorVentanas());
-    // }
-	
-	
+    @Before
+    public void setUp() {
+        gestorVentanas = mock(GestorVentanas.class);
+        exampleClient = mock(ExampleClient.class);
+    }
+
+    @Test
+    public void testMain() {
+        String[] args = {"localhost", "8080"};
+        Main.main(args);
+        
+        assertNotNull(Main.getExampleClient());
+        assertNotNull(Main.getGestorVentanas());
+    }
 }
+
--- src/test/java/es/deusto/spq/server/ResourceTest.java
+++ src/test/java/es/deusto/spq/server/ResourceTest.java
@@ -1,17 +1,41 @@
 package es.deusto.spq.server;
 
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import es.deusto.spq.pojo.ReservaData;
-import es.deusto.spq.server.jdo.Reserva;
-
-import static org.junit.Assert.*;
-
+import java.util.HashMap;
 import java.util.List;
+import static org.hamcrest.CoreMatchers.not;
 
-import javax.ws.rs.core.GenericType;
+import javax.jdo.Query;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Transaction;
 import javax.ws.rs.core.Response;
+import static org.mockito.ArgumentMatchers.eq;
 
+import static org.junit.Assert.assertNotNull;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import static org.junit.Assert.fail;
+import es.deusto.spq.pojo.UserData;
+import es.deusto.spq.server.jdo.Reserva;
+import es.deusto.spq.server.jdo.User;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ResourceTest {
 
 	// @BeforeClass
@@ -28,7 +52,80 @@ public class ResourceTest {
 	// 	r2 = new Resource();
 	// }
 
-	@Test
+
+
+    @InjectMocks
+    private Resource loginService;
+
+    @Mock
+    private org.apache.logging.log4j.Logger logger;
+
+    @Mock
+    private PersistenceManager pm;
+
+    @Mock
+    private Transaction tx;
+
+    @Mock
+    private HashMap<Long, User> serverState;
+
+    @Before
+    public void setUp() {
+        when(pm.getObjectById(eq(User.class), anyString())).thenAnswer(invocation -> {
+            String id = invocation.getArgument(1);
+            if ("testId".equals(id)) {
+                return new User("testId", "testPassword");
+            } else {
+                throw new javax.jdo.JDOObjectNotFoundException();
+            }
+        });
+        when(tx.isActive()).thenReturn(true);
+    }
+    
+    
+    /*
+    @Test
+    public void testLoginUser_validUser_success() {
+        User user1 = new User("testId", "testPassword", false);
+        when(pm.getObjectById(User.class, user1.getId())).thenReturn(user1);
+        UserData userData1 = new UserData(user1.getId(), user1.getPassword(), false);
+        Response response1 = loginService.loginUser(userData1);
+        assertEquals(Response.Status.OK.getStatusCode(), response1.getStatus());
+    }
+     */
+
+    @Test
+    public void testLoginUser_invalidPassword_unauthorized() {
+        User user2 = new User("testId", "testPassword");
+        when(pm.getObjectById(User.class, user2.getId())).thenReturn(user2);
+        UserData userData2 = new UserData(user2.getId(), "wrongPassword", false);
+        Response response2 = loginService.loginUser(userData2);
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response2.getStatus());
+    }
+
+    @Test
+    public void testLoginUser_invalidUser_unauthorized() {
+        UserData userData3 = new UserData("invalidId", "testPassword", false);
+        Response response3 = loginService.loginUser(userData3);
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response3.getStatus());
+    }
+
+    /* 
+    @Test
+    public void testLoginUser_error_internalServerError() {
+        User user1 = new User("testId", "testPassword");
+        UserData userData4 = new UserData(user1.getId(), user1.getPassword(), false);
+    
+        try {
+            Response response4 = loginService.loginUser(userData4);
+            fail("Expected an RuntimeException to be thrown");
+        } catch (RuntimeException e) {
+            // If the exception is caught, the test passes.
+        }
+    }
+    */
+    
+    @Test
 	public void sayMessage() {
 		assertEquals(5,5);
 	}
@@ -36,10 +133,7 @@ public class ResourceTest {
 	public void registerUser() {
 		
 	}
-	@Test
-	public void loginUser() {
-
-	}
+    
 	@Test
 	public void logout(){
 		
@@ -63,22 +157,22 @@ public class ResourceTest {
 
 	}
 	
-	// @Test
-    // public void getReservas() {
-    //     // Crear una instancia de la clase que contiene el método getReservas
-    //     Resource reservasResource = new Resource();
+	 @Test
+     public void getReservas() {
+         // Crear una instancia de la clase que contiene el método getReservas
+         Resource reservasResource = new Resource();
 
-    //     // Llamar al método getReservas y almacenar la respuesta
-    //     Response response = reservasResource.getReservas();
+         // Llamar al método getReservas y almacenar la respuesta
+         Response response = reservasResource.getReservas();
 
-    //     // Verificar si la respuesta es correcta (HTTP 200 OK)
-    //     assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+         // Verificar si la respuesta es correcta (HTTP 200 OK)
+         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 
-    //     // Verificar si el contenido de la respuesta es el esperado (lista de reservas)
-    //     List<Reserva> reservas = (List<Reserva>) response.getEntity();
-    //     assertNotNull(reservas);
-    //     // Aquí se pueden agregar más verificaciones para los datos de las reservas si en un futuro se consideras necesario
-    // }
+         // Verificar si el contenido de la respuesta es el esperado (lista de reservas)
+         List<Reserva> reservas = (List<Reserva>) response.getEntity();
+         assertNotNull(reservas);
+         // Aquí se pueden agregar más verificaciones para los datos de las reservas si en un futuro se consideras necesario
+     }
 
 
 	@Test
