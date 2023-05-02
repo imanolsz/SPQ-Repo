4cae5ae671d0af3a03b63bfecb55d2c43bc82a78
Ignacio González <ignacio.gonlez@opendeusto.es>
2023-05-02 09:58:52 +0200

    teses unitarios directmessage

--- src/test/java/es/deusto/spq/pojo/DirectMessageTest.java
+++ src/test/java/es/deusto/spq/pojo/DirectMessageTest.java
@@ -1,11 +1,62 @@
 package es.deusto.spq.pojo;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.*;
 
 public class DirectMessageTest {
 
+	private DirectMessage directMessage;
+	private UserData userData;
+	private MessageData messageData;
+
     @Before
 	public void setUp() throws Exception {
 		// Code executed before each test
+		directMessage = new DirectMessage();
+		userData = new UserData();
+		messageData = new MessageData();
+		
+
 	}
+
+	@Test
+	public void testSetAndGetUserData() {
+		directMessage.setUserData(userData);
+		assertNotNull(directMessage.getUserData());
+	}
+
+	@Test
+	public void testSetAndGetMessageData() {
+		directMessage.setMessageData(messageData);
+		assertNotNull(directMessage.getMessageData());
+	}
+
+	@Test
+	public void testConstructor() {
+		DirectMessage directMessage = new DirectMessage();
+		assertNotNull(directMessage);
+	}
+
+	@Test
+	public void testGetAndSetUserData() {
+		UserData userData = new UserData();
+		userData.setId("user");
+		directMessage.setUserData(userData);
+		assertEquals(userData, directMessage.getUserData());
+	}
+
+	@Test
+	public void testGetAndSetMessageData() {
+		MessageData messageData = new MessageData();
+		messageData.setMessage("Hello World!");
+		directMessage.setMessageData(messageData);
+		assertEquals(messageData, directMessage.getMessageData());
+	}
+
+
+
+
 }
