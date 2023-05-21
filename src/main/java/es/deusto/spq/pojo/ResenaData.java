25951e1098291509e080a28263d77b8951f7c38d
luis-suarez-cuartas <luissuarezcuartas@opendeusto.es>
2023-05-21 17:29:16 +0200

    resena Data

---
+++ src/main/java/es/deusto/spq/pojo/ResenaData.java
@@ -0,0 +1,15 @@
+package es.deusto.spq.pojo;
+
+public class ResenaData {
+    private String resena;
+    public ResenaData(String resena) {
+        this.resena = resena;
+    }
+    public String getResena() {
+        return resena;
+    }
+
+    public void setResena(String resena) {
+        this.resena = resena;
+    }
+}
